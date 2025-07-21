801fb0db9cb93867b024154135b3918e484ce87a
Kanish1411 <kanishkark1411@gmail.com>
2025-07-21 10:14:33 +0530

    bash file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 ﻿# Lightnigh-Network-Privacy
+
+# core running
+1) init
+2) initial_prob
+3) entropy_before
+4) Transaction 
+5) Trx_amt_analysis
+6) Prob_after_Amt
+7) entropy_analysis
--- init.py
+++ init.py
@@ -2,15 +2,42 @@ import json
 import pickle
 import networkx as nx
 from lnbalance import *
+import os
+import sys
 
-def init_normal(amt=0):
+def edge_cost(u, v, data, trx_amt):
+    cost = (trx_amt * ((data.get("prop_fee", 0) / 1e6) + (data.get("timelock", 0) * data.get("rf", 1e-9)))) +  data.get("base_fee", 0) + data.get("bias", 1)
+    return cost
+
+def calculate_total_fee(path, trx_amt, G):
+    """
+    Computes the total fee for the transaction by working backwards from the destination.
+    """
+    total_fee = 0
+    amt_needed = trx_amt
+
+    for i in range(len(path) - 1, 0, -1):
+        v, u = path[i], path[i - 1] 
+
+        edge_data = G[u][v]
+        base_fee = edge_data.get("base_fee", 0)
+        prop_fee = amt_needed * ((edge_data.get("prop_fee", 0) / 1e6)+(edge_data.get("timelock", 0) * edge_data.get("rf", 1e-9)))
+
+        hop_fee = base_fee + prop_fee + edge_data.get("bias", 1)
+        total_fee += hop_fee
+        amt_needed += hop_fee  
+        print(f"for hop {u} to {v} is {hop_fee} = {base_fee} {prop_fee}")
+    return total_fee
+
+def init_normal(amt,graph_file, netstats_file):
     G=nx.DiGraph()
-    with open("Data/Graph_new.json", encoding="utf-8") as f:
+    with open(graph_file, encoding="utf-8") as f:
         data = json.load(f)
     for node in data["nodes"]:
         G.add_node(node["pub_key"])
-    with open("Data/netstats.json", encoding="utf-8") as f:
-        stats_data=json.load(f)
+
+    with open(netstats_file, encoding="utf-8") as f:
+        stats_data = json.load(f)
     base_fee=stats_data["latest"]["avg_base_fee_mtokens"]
     prop_fee=stats_data["latest"]["avg_fee_rate"]
     timelock=50
@@ -55,15 +82,18 @@ def init_normal(amt=0):
         pass
     return G
 
-def init_unif_normal(amt):
+def init_unif_normal(amt,graph_file, netstats_file):
     global G
     G=nx.DiGraph()
-    with open("Data/Graph_new.json", encoding="utf-8") as f:
+    with open(graph_file, encoding="utf-8") as f:
         data = json.load(f)
+        print("graph file loaded")
     for node in data["nodes"]:
         G.add_node(node["pub_key"])
-    with open("Data/netstats.json", encoding="utf-8") as f:
-        stats_data=json.load(f)
+
+    with open(netstats_file, encoding="utf-8") as f:
+        stats_data = json.load(f)
+    print("eoroperp")
     base_fee=stats_data["latest"]["avg_base_fee_mtokens"]
     prop_fee=stats_data["latest"]["avg_fee_rate"]
     timelock=50
@@ -109,15 +139,16 @@ def init_unif_normal(amt):
         pass
     return G
 
-def init_Bimodal(amt):
+def init_Bimodal(amt,graph_file, netstats_file):
     global G
     G=nx.DiGraph()
-    with open("Data/Graph_new.json", encoding="utf-8") as f:
+    with open(graph_file, encoding="utf-8") as f:
         data = json.load(f)
     for node in data["nodes"]:
         G.add_node(node["pub_key"])
-    with open("Data/netstats.json", encoding="utf-8") as f:
-        stats_data=json.load(f)
+    with open(netstats_file, encoding="utf-8") as f:
+        stats_data = json.load(f)
+
     base_fee=stats_data["latest"]["avg_base_fee_mtokens"]
     prop_fee=stats_data["latest"]["avg_fee_rate"]
     timelock=50
@@ -167,4 +198,17 @@ def init_Bimodal(amt):
         pass
     return G
 
+if len(sys.argv) < 2:
+    print("Usage: python3 script.py <graph_file> [netstats_file]")
+    sys.exit(1)
 
+graph_file = sys.argv[1]
+netstats_file = sys.argv[2] if len(sys.argv) > 2 else "Data/netstats.json"
+output_folder = "Graphs"
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+    for amt in [10, 100, 1000, 10000]:
+        print("aaa")
+        init_unif_normal(amt, graph_file, netstats_file)
+else:
+    print(f"Folder '{output_folder}' already exists. Skipping execution.")
